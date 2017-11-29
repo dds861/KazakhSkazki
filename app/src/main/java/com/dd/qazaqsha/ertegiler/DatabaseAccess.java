4354356d5d89fd835bf0e0052b1adcb90e7e5304
dds861 <dds861@gmail.com>
2017-11-29 13:54:09 +0100

    refatoring1

--- app/src/main/java/com/dd/qazaqsha/ertegiler/DatabaseAccess.java
+++ app/src/main/java/com/dd/qazaqsha/ertegiler/DatabaseAccess.java
@@ -11,9 +11,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DatabaseAccess extends AppCompatActivity {
+    private static DatabaseAccess instance;
     private SQLiteOpenHelper openHelper;
     private SQLiteDatabase database;
-    private static DatabaseAccess instance;
+
+    public DatabaseAccess() {
+    }
 
     private DatabaseAccess(Context context) {
         this.openHelper = new DatabaseOpenHelper(context);
